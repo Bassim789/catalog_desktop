bb90a8f0c02032e95be0f03ca9f62b355774f095
Bassim <bassim.matar@gmail.com>
2019-11-22 20:04:56 +0100

    rename test

--- process/Vars_meta_test.py
+++ process/Vars_meta_test.py
@@ -1,11 +1,11 @@
 import pandas
 import json
-from Variables import Variables
+from Vars_meta import Vars_meta
 
 def test_clean_num():
 
   def get_clean_num(num):
-    return Variables.clean_num(pandas.DataFrame([num])[0][0])
+    return Vars_meta.clean_num(pandas.DataFrame([num])[0][0])
 
   assert get_clean_num('ok') == 'ok'
   assert get_clean_num('') == ''
@@ -19,8 +19,8 @@ def test_clean_num():
   assert get_clean_num(0.42) == 0.42
 
 def test_percent():
-  assert Variables.percent(140, 200) == 70
-  assert Variables.percent(20, 50) == 40
-  assert Variables.percent(36, 100) == 36
-  assert Variables.percent(0, 30) == 0
-  assert Variables.percent(10, 0) == False
+  assert Vars_meta.percent(140, 200) == 70
+  assert Vars_meta.percent(20, 50) == 40
+  assert Vars_meta.percent(36, 100) == 36
+  assert Vars_meta.percent(0, 30) == 0
+  assert Vars_meta.percent(10, 0) == False
