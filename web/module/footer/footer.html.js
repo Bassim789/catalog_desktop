b39e18c0f6375868f36e4b8752e74bb80981aa7c
Bassim <bassim.matar@gmail.com>
2020-04-22 16:38:31 +0200

    update footer github link

--- web/module/footer/footer.html.js
+++ web/module/footer/footer.html.js
@@ -2,6 +2,6 @@ template.footer = `
 <div id="scan_info">
   Dernière vérification {{scan_time_ago}}
   <br>
-  <i>avec <a href="https://github.com/bassim-matar/catalog_desktop" target="_blanck">Datadir</a><i>
+  <i>avec <a href="https://github.com/bassim-matar/datadir" target="_blanck">Datadir</a><i>
 </div>
 `;
